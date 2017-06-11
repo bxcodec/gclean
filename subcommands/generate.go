4278daa338de126bd93d6300797daf3e21f7dfe3
Iman <iman@Imans-MacBook-Air.local>
2017-06-11 23:07:36 +0700

    prototype models generator completed

--- subcommands/generate.go
+++ subcommands/generate.go
@@ -1,25 +1,53 @@
 package subcommands
 
-import "github.com/spf13/cobra"
-import "fmt"
-import "strings"
+import (
+	"strconv"
+	"time"
 
-
+	"github.com/spf13/cobra"
+)
 
 type Subs struct {
-
-}
-func (s *Subs)generate(cmd *cobra.Command, args []string) {
-	fmt.Println("HELLO : ", strings.Join(args, " "))
 }
 
-func (s *Subs)AddGenerate(root *cobra.Command)  {
+func (s *Subs) generate(cmd *cobra.Command, args []string) {
+	dataList := make([]*ModelGenerator, 0)
 
-  var cmdDemo = &cobra.Command{
-  	Use:   "generate",
-  	Short: "Generate your Golang projects",
-  	Run: s.generate,
-  }
+	id := &Attribute{
+		Name: "ID",
+		Type: "int64",
+	}
+	title := &Attribute{
+		Name: "Title",
+		Type: "string",
+	}
+	content := &Attribute{
+		Name: "Content",
+		Type: "string",
+	}
 
-  root.AddCommand(cmdDemo)
+	for i := 1; i <= 5; i++ {
+
+		dataList = append(dataList, &ModelGenerator{
+			ModelName:  "Model" + strconv.Itoa(i),
+			Attributes: []*Attribute{id, title, content},
+			TimeStamp:  time.Now(),
+		})
+	}
+
+	for k, v := range dataList {
+		s.generateModels(v, k)
+	}
+
+}
+
+func (s *Subs) AddGenerate(root *cobra.Command) {
+
+	var cmdDemo = &cobra.Command{
+		Use:   "generate ",
+		Short: "Generate your Golang projects",
+		Run:   s.generate,
+	}
+
+	root.AddCommand(cmdDemo)
 }
---
+++ subcommands/generate_models.go
@@ -0,0 +1,53 @@
+package subcommands
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"text/template"
+
+	"github.com/Masterminds/sprig"
+
+	// "html/template"
+	"time"
+)
+
+type ModelGenerator struct {
+	TimeStamp  time.Time
+	ModelName  string
+	Attributes []*Attribute
+}
+
+type Attribute struct {
+	Name string
+	Type string
+}
+
+func (s *Subs) generateModels(dataSend *ModelGenerator, k int) {
+
+	var err error
+
+	temp, err := template.New("").Funcs(sprig.TxtFuncMap()).ParseFiles("template/models.tpl")
+
+	if err != nil {
+		fmt.Println("GALGAL", err)
+		os.Exit(0)
+	}
+
+	pathP := "models/"
+	if _, er := os.Stat(pathP); os.IsNotExist(er) {
+		os.MkdirAll(pathP, os.ModePerm)
+	}
+	f, err := os.Create(pathP + "sample" + strconv.Itoa(k) + ".go")
+	if err != nil {
+		fmt.Println("Erorr")
+	}
+
+	defer f.Close()
+	err = temp.ExecuteTemplate(f, "models.tpl", dataSend)
+
+	if err != nil {
+		fmt.Println("ERROR ", err)
+		os.Exit(0)
+	}
+}
